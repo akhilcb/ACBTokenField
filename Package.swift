2668a08260dd492b223812c6b5992587c783473e
Akhil <akhilcb@yahoo.com>
2018-02-19 15:42:43 -0800

    Updated swift package file

--- Package.swift
+++ Package.swift
@@ -1,5 +1,19 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "ACBTokenField"
+    name: "ACBTokenField",
+    products: [
+        .library(
+            name: "ACBTokenField",
+            targets: ["ACBTokenField"]),
+        ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "ACBTokenField",
+            dependencies: [])
+        ],
+    swiftLanguageVersions: [3]
 )
